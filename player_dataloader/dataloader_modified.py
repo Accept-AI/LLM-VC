2fdfa1b2562ca9b1ff5683ebc9e4e3d0aad7172b
习泽宇 <57516993+Xiyu-AI@users.noreply.github.com>
2024-11-11 11:32:17 +0800

    Add files via upload

---
+++ player_dataloader/dataloader_modified.py
@@ -0,0 +1,39 @@
+from .volleyball import *
+from .nba_modified import *
+
+
+TRAIN_SEQS_VOLLEY = [1, 3, 6, 7, 10, 13, 15, 16, 18, 22, 23, 31, 32, 36, 38, 39, 40, 41, 42, 48, 50, 52, 53, 54]
+VAL_SEQS_VOLLEY = [0, 2, 8, 12, 17, 19, 24, 26, 27, 28, 30, 33, 46, 49, 51]
+TEST_SEQS_VOLLEY = [4, 5, 9, 11, 14, 20, 21, 25, 29, 34, 35, 37, 43, 44, 45, 47]
+
+
+def read_dataset(args):
+    # data_path = args.data_path + 'NBA_dataset'
+    # image_path = data_path + "/videos"
+    image_path = '/home/xzy/xzy_nba/Players'  #
+
+    train_playerid_path = '/home/xzy/xzy_nba/LLM_VC/Player_identify/Save/D_train.json'
+    test_playerid_path = '/home/xzy/xzy_nba/LLM_VC/Player_identify/Save/D_test.json'
+
+    train_actionid_path = '/home/xzy/xzy_nba/LLM_VC/Player_identify/Save/E_action_train.json'
+    test_actionid_path = '/home/xzy/xzy_nba/LLM_VC/Player_identify/Save/E_action_test.json'
+    #test_id_path = '/home/xzy/xzy/caption/LLM_VC/Player_identify/Save/D_all.json'
+
+    train_ids = read_ids(train_playerid_path)
+    test_ids = read_ids(test_playerid_path)
+
+    train_data = nba_read_annotations(train_playerid_path, train_actionid_path, train_ids)
+    #print(train_data)
+    #train_data = nba_read_annotations(image_2022_path, train_ids)
+    train_frames = nba_all_frames(train_data)
+
+    test_data = nba_read_annotations(test_playerid_path, test_actionid_path, test_ids)
+    test_frames = nba_all_frames(test_data)
+
+    train_set = NBADataset(train_frames, train_data, image_path, args)
+    test_set = NBADataset(test_frames, test_data, image_path, args)
+
+
+    print("%d train samples and %d test samples" % (len(train_frames), len(test_frames)))
+
+    return train_set, test_set
---
+++ player_dataloader/nba.py
@@ -0,0 +1,124 @@
+import torch
+import torch.utils.data as data
+import torchvision.transforms as transforms
+import json
+import os
+
+import numpy as np
+import random
+from PIL import Image
+
+ACTIVITIES = ['2p-succ.', '2p-fail.-off.', '2p-fail.-def.',
+              '2p-layup-succ.', '2p-layup-fail.-off.', '2p-layup-fail.-def.',
+              '3p-succ.', '3p-fail.-off.', '3p-fail.-def.']
+
+with open("/home/xzy/NBA/LLM_VC/Player_identify/Save/D_all_name_label.json", encoding='utf-8') as Player_list:
+    result_player_list = json.load(Player_list)
+PLAYERS = result_player_list['all']
+print("PLAYERS: ", len(PLAYERS))
+
+def read_ids(path):
+    with open(path, encoding='utf-8') as data_f:
+        result_data = json.load(data_f)
+    data_list = list(result_data.keys())
+    return data_list
+
+def nba_read_annotations(path, seqs):
+    labels = {}
+    name_to_id = {name: i for i, name in enumerate(PLAYERS)}
+    with open(path, encoding='utf-8') as data_f:
+        result_data = json.load(data_f)
+
+    for sid in seqs:
+        annotations = {}
+        player_label = result_data[sid]
+        player_id = name_to_id[player_label]
+        annotations[sid] = {
+            'player_id': player_id,
+        }
+        labels[sid] = annotations
+
+    return labels
+
+
+
+def nba_all_frames(labels):
+    frames = []
+    for sid, anns in labels.items():
+        for fid, ann in anns.items():
+            frames.append((sid, ann))
+    #print("frames: ", frames)
+    return frames
+
+
+class NBADataset(data.Dataset):
+    """
+    Volleyball Dataset for PyTorch
+    """
+    def __init__(self, frames, anns, image_path, args):
+        super(NBADataset, self).__init__()
+        self.frames = frames
+        self.anns = anns
+        self.image_path = image_path
+        self.image_size = (args.image_width, args.image_height)
+        self.image_width = args.image_width
+        self.image_height = args.image_height
+        self.num_frame = args.num_frame
+
+        self.transform = transforms.Compose([
+            transforms.Resize((args.image_height, args.image_width)),
+            transforms.ToTensor(),
+            transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+        ])
+
+    def __getitem__(self, idx):
+        #frames = self.select_frames(self.frames[idx])
+        samples = self.load_samples(self.frames[idx])
+
+        return samples
+
+    def __len__(self):
+        return len(self.frames)
+
+    def load_samples(self, frames):
+        images, players = [], []
+        #print("frames: ", frames)
+        vid, sid = frames
+        #print(vid)
+        path = os.path.join(self.image_path, vid)
+        # 获取该目录下所有文件，存入列表中
+        fileList = os.listdir(path)
+        # get_key是sorted函数用来比较的元素，该处用lambda表达式替代函数。
+        #get_key = lambda i: int(i.split('.')[0])
+        new_sort = sorted(fileList, key=lambda i: int((i.split('.')[0]).split('_')[1]))
+        for i_image in new_sort:
+            i_path = os.path.join(self.image_path, vid, i_image)
+            #print("i_path: ", i_path)
+            img = Image.open(i_path)
+            img = self.transform(img)
+            images.append(img)
+            #players.append(sid['player_id'])
+
+        images = torch.stack(images)
+        if self.num_frame < images.shape[0]:  # 20 阈值
+            images = images[:self.num_frame]
+        i_l, _, _, _ = images.shape
+        padded_video = np.zeros((self.num_frame, 3, self.image_height, self.image_width))
+        padded_video[:i_l, :, :, :] = images
+        #print("images: ", images.shape)  #images:  torch.Size([X, 3, 360, 640])
+        #print("padded_video: ", padded_video.shape) # padded_video:  torch.Size([20, 3, 360, 640])
+        video_mask = np.zeros((1, self.num_frame), dtype=np.long)
+        video_mask[0][:i_l] = [1] * i_l
+        #print("video_mask: ", video_mask.shape) # video_mask:  (1, 20)
+
+        players = [sid['player_id']] * self.num_frame
+
+        players = np.array(players, dtype=np.int32)
+        #print("players: ", players.shape)  # players:  (20,)
+
+
+        # convert to pytorch tensor
+        players = torch.from_numpy(players).long()
+
+
+        return padded_video, video_mask, players
